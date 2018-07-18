7fabc25fb32fa61fc148c3e6a1f3bc18881def21
Vadim Hleif <vadzim.hleif@gmail.com>
2018-07-18 12:24:34 +0300

    Migrate readme yaml (#2)

--- output.tf
+++ output.tf
@@ -1,15 +1,19 @@
 output "key_arn" {
-  value = "${aws_kms_key.default.arn}"
+  value       = "${aws_kms_key.default.arn}"
+  description = "Key ARN"
 }
 
 output "key_id" {
-  value = "${aws_kms_key.default.key_id}"
+  value       = "${aws_kms_key.default.key_id}"
+  description = "Key ID"
 }
 
 output "alias_arn" {
-  value = "${aws_kms_alias.default.arn}"
+  value       = "${aws_kms_alias.default.arn}"
+  description = "Alias ARN"
 }
 
 output "alias_name" {
-  value = "${aws_kms_alias.default.name}"
+  value       = "${aws_kms_alias.default.name}"
+  description = "Alias name"
 }
