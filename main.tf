6ecd8c2851afa3a2fe616fdc13b0b231b1dfd142
Rohit Verma <rohit.verma@niki.ai>
2019-06-21 14:55:23 +0530

    upgraded-tf

--- main.tf
+++ main.tf
@@ -1,21 +1,22 @@
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
-  namespace  = "${var.namespace}"
-  stage      = "${var.stage}"
-  name       = "${var.name}"
-  attributes = "${var.attributes}"
-  delimiter  = "${var.delimiter}"
-  tags       = "${var.tags}"
+  source     = "git::https://github.com/rverma-nikiai/terraform-null-label.git?ref=master"
+  namespace  = var.namespace
+  stage      = var.stage
+  name       = var.name
+  attributes = var.attributes
+  delimiter  = var.delimiter
+  tags       = var.tags
 }
 
 resource "aws_kms_key" "default" {
-  description             = "${var.description}"
-  deletion_window_in_days = "${var.deletion_window_in_days}"
-  enable_key_rotation     = "${var.enable_key_rotation}"
-  tags                    = "${module.label.tags}"
+  description             = var.description
+  deletion_window_in_days = var.deletion_window_in_days
+  enable_key_rotation     = var.enable_key_rotation
+  tags                    = module.label.tags
 }
 
 resource "aws_kms_alias" "default" {
-  name          = "${coalesce(var.alias, format("alias/%v", module.label.id))}"
-  target_key_id = "${aws_kms_key.default.id}"
+  name          = coalesce(var.alias, format("alias/%v", module.label.id))
+  target_key_id = aws_kms_key.default.id
 }
+
