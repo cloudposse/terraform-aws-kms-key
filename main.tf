ca15d92b069bb4878dc7155c71a027164596ad20
aknysh <andriy.knysh@gmail.com>
2019-06-28 14:52:56 -0400

    Convert to TF 0.12. Add tests. Add Codefresh test pipeline

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ module "label" {
 resource "aws_kms_key" "default" {
   deletion_window_in_days = var.deletion_window_in_days
   enable_key_rotation     = var.enable_key_rotation
-  description             = var.description
-  tags                    = module.label.tags
+  #description             = var.description
+  tags = module.label.tags
 }
 
 resource "aws_kms_alias" "default" {
