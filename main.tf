fb01e7ed47893632d549bd35a86f2ba13d96dea5
Igor Rodionov <goruha@gmail.com>
2019-08-05 18:26:46 +0600

    Added default policy

--- main.tf
+++ main.tf
@@ -8,12 +8,36 @@ module "label" {
   tags       = "${var.tags}"
 }
 
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "default" {
+  statement {
+    sid       = "Enable IAM User Permissions"
+    effect    = "Allow"
+    actions   = ["kms:*"]
+    resources = ["*"]
+
+    principals {
+      type = "AWS"
+
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
+      ]
+    }
+  }
+}
+
+module "policy" {
+  source           = "git::https://github.com/cloudposse/terraform-aws-iam-policy-document-aggregator.git?ref=tags/0.1.2"
+  source_documents = ["${data.aws_iam_policy_document.default.json}" ,"${var.policy}"]
+}
+
 resource "aws_kms_key" "default" {
   description             = "${var.description}"
   deletion_window_in_days = "${var.deletion_window_in_days}"
   enable_key_rotation     = "${var.enable_key_rotation}"
   tags                    = "${module.label.tags}"
-  policy                  = "${var.policy}"
+  policy                  = "${module.policy.result_document}"
 }
 
 resource "aws_kms_alias" "default" {
