6dd1ce325be9889e181ee23198aee6b85d3f6925
Andriy Knysh <aknysh@users.noreply.github.com>
2020-09-01 23:07:22 -0400

    Update to `context.tf`. Update Terratest to Golang modules (#17)
    * Update to `context.tf`. Update Terratest to Golang modules
    
    * Update to `context.tf`. Update Terratest to Golang modules

--- main.tf
+++ main.tf
@@ -1,25 +1,14 @@
-module "label" {
-  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.17.0"
-  namespace   = var.namespace
-  stage       = var.stage
-  environment = var.environment
-  name        = var.name
-  attributes  = var.attributes
-  delimiter   = var.delimiter
-  tags        = var.tags
-}
-
 resource "aws_kms_key" "default" {
-  count                   = var.enabled == true ? 1 : 0
+  count                   = module.this.enabled ? 1 : 0
   deletion_window_in_days = var.deletion_window_in_days
   enable_key_rotation     = var.enable_key_rotation
   policy                  = var.policy
-  tags                    = module.label.tags
+  tags                    = module.this.tags
   description             = var.description
 }
 
 resource "aws_kms_alias" "default" {
-  count         = var.enabled == true ? 1 : 0
-  name          = coalesce(var.alias, format("alias/%v", module.label.id))
+  count         = module.this.enabled ? 1 : 0
+  name          = coalesce(var.alias, format("alias/%v", module.this.id))
   target_key_id = join("", aws_kms_key.default.*.id)
 }
