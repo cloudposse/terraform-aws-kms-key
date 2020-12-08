8ebdc5b6ee9e96be8906051a2cb3a8d872a9106b
dmattia <david@transcend.io>
2020-12-08 11:09:30 -0600

    upgrade version reqs

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
+      version = ">= 3.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = ">= 1.2"
+      version = ">= 1.3"
     }
     null = {
       source  = "hashicorp/null"
