f7a97412dafb78b588ebb556e01fafb5e2dd44ad
David Mattia <8922077+dmattia@users.noreply.github.com>
2020-12-09 10:33:21 -0600

    work for terraform 0.14 (#20)
    * work for terraform 0.14
    
    * update example modules
    
    * Updated README.md
    
    * change source format
    
    * req tf 0.13 minimum
    
    * upgrade version reqs
    
    * Updated README.md
    
    * Executed 'terraform fmt'
    
    Co-authored-by: dmattia <david@transcend.io>
    Co-authored-by: actions-bot <58130806+actions-bot@users.noreply.github.com>

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -1,9 +1,18 @@
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = ">= 0.13"
 
   required_providers {
-    aws   = ">= 2.0"
-    local = ">= 1.2"
-    null  = ">= 2.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 1.3"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.0"
+    }
   }
 }
