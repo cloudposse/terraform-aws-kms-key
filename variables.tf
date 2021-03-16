38aaed50c618a7939c3f4f91ec547586f4631e4a
Frank Klaassen <frank@cloudright.nl>
2021-03-16 10:21:57 +0100

    add customer_master_key_spec variable

--- variables.tf
+++ variables.tf
@@ -33,3 +33,9 @@ variable "key_usage" {
   default     = "ENCRYPT_DECRYPT"
   description = "Specifies the intended use of the key. Valid values: `ENCRYPT_DECRYPT` or `SIGN_VERIFY`."
 }
+
+variable "customer_master_key_spec" {
+  type        = string
+  default     = "SYMMETRIC_DEFAULT"
+  description = "Specifies whether the key contains a symmetric key or an asymmetric key pair and the encryption algorithms or signing algorithms that the key supports. Valid values: `SYMMETRIC_DEFAULT`, `RSA_2048`, `RSA_3072`, `RSA_4096`, `ECC_NIST_P256`, `ECC_NIST_P384`, `ECC_NIST_P521`, or `ECC_SECG_P256K1`."
+}
