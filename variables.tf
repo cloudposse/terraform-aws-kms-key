b3158a1e2ee85ec75d9bd5705ebf9ac5b367fc95
nitrocode <nitrocode@users.noreply.github.com>
2021-11-05 08:21:23 -0500

    Add multi_region

--- variables.tf
+++ variables.tf
@@ -39,3 +39,9 @@ variable "customer_master_key_spec" {
   default     = "SYMMETRIC_DEFAULT"
   description = "Specifies whether the key contains a symmetric key or an asymmetric key pair and the encryption algorithms or signing algorithms that the key supports. Valid values: `SYMMETRIC_DEFAULT`, `RSA_2048`, `RSA_3072`, `RSA_4096`, `ECC_NIST_P256`, `ECC_NIST_P384`, `ECC_NIST_P521`, or `ECC_SECG_P256K1`."
 }
+
+variable "multi_region" {
+  type        = bool
+  default     = false
+  description = "Indicates whether the KMS key is a multi-Region (true) or regional (false) key."
+}
